6439bf3fdd5216f87340019a512068b1cb208459
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2017-10-05 08:55:51 -0300

    implementado list erros de customer-new

--- src/app/modules/user/customer-new/customer-new.component.ts
+++ src/app/modules/user/customer-new/customer-new.component.ts
@@ -8,7 +8,7 @@ import { UserService } from '../user.service';
   selector: 'app-customer-new',
   templateUrl: './customer-new.component.html',
   styleUrls: ['./customer-new.component.css'],
-  providers:[UserService]
+  providers: [UserService]
 })
 export class CustomerNewComponent implements OnInit {
 
@@ -29,6 +29,8 @@ export class CustomerNewComponent implements OnInit {
     phone: "",
     userId: this.userId
   }
+
+  public erros = [];
   constructor(private _toastr: ToastrService, private _userService: UserService, private _route: Router) { }
 
   ngOnInit() {
@@ -59,34 +61,20 @@ export class CustomerNewComponent implements OnInit {
       this._toastr.error('Favor informe o o endereco ;)', 'Op´s');
       return;
     } else {
-
-       this._userService.postCustomer(this.customer).subscribe((resp:any) => {
-
-        if(resp.success == true){
-          this._toastr.success('Sucesso !!','Seu cadastro esta completo');
+      this._userService.postCustomer(this.customer).subscribe((resp: any) => {
+        if (resp.success == true) {
+          this._toastr.success('Sucesso !!', 'Seu cadastro esta completo');
           this._route.navigate(['/user-details']);
-        } else {
-          this._toastr.error('Ops','Seu formulario contem erros');
-        }         
-         console.log(resp);      
-       });
+        } else if (resp.success == false) {
+          this._toastr.error('Ops', 'Houve um erro tente novamente');
+          this.erros = resp.error;
+        }
+        console.log(resp.error);
+      });
     }
   }
 
-  clear() {
-   
-    this.customer.cep = "";
-    this.customer.city = "";
-    this.customer.complement = "";
-    this.customer.district = "";
-    this.customer.number = 0;
-    this.customer.street = "";
-    this.customer.state = "";
-    this.customer.dateBirthday = ""
-    this.customer.document = "0";
-    this.customer.gender = false;
-    this.customer.phone = "";
-  };
+  
 }
 
 
